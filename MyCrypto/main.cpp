97de62ec320d167ed20f02eb75d967bf76d75eb9
Li <91820141+leerh123@users.noreply.github.com>
2024-02-11 16:05:00 +0800

    main文件

---
+++ MyCrypto/main.cpp
@@ -0,0 +1,13 @@
+#include"LargeInt.h"
+#include"Mod.h"
+#include"AES.h"
+#include"hash.h"
+#include"RSA.h"
+#include<iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "hello MyCyrpto!" << endl;
+}
+
